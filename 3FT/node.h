66b0dd859683d58ec3b8fcde4c3da120db3f2c8b
Diane Yang <dianey@princeton.edu>
2020-11-05 15:21:16 -0500

    some new comments

--- 3FT/node.h
+++ 3FT/node.h
@@ -10,27 +10,30 @@
 #include "a4def.h"
 
 /*
-   a Node is an object that contains a path payload and references to
-   the Node's parent (if it exists) and children (if they exist).
+   A Node is an object that contains a path payload and references to
+   the Node's parent (if it exists). If the Node is a directory, it
+   contains references to children (if they exist). If the Node is a
+   file, it contains references to the contents and length of the file.
 */
 typedef struct node* Node;
+
 typedef enum {DIRECTORY, FILE} nodeType;
 
 
 /*
-   Given a parent Node and a directory string dir, returns a new
+   Given a parent Node and a directory/file string name, returns a new
    Node structure or NULL if any allocation error occurs in creating
    the node or its fields.
 
    The new structure is initialized to have its path as the parent's
-   path (if it exists) prefixed to the directory string parameter,
+   path (if it exists) prefixed to the name string parameter,
    separated by a slash. It is also initialized with its parent link
    as the parent parameter value (but the parent itself is not changed
-   to link to the new Node.  The children links are initialized but
-   do not point to any children.
+   to link to the new Node. If the Node is a directory, the  children 
+   links are initialized but do not point to any children.
 */
 
-Node Node_create(const char* dir, Node parent, nodeType type);
+Node Node_create(const char* name, Node parent, nodeType type);
 
 /*
   Destroys the entire hierarchy of Nodes rooted at n,
@@ -44,7 +47,8 @@ size_t Node_destroy(Node n);
 /*
   Compares node1 and node2 based on their paths.
   Returns <0, 0, or >0 if node1 is less than,
-  equal to, or greater than node2, respectively.
+  equal to, or greater than node2, respectively. A file Node will
+  always be less than to a directory Node.
 */
 int Node_compare(Node node1, Node node2);
 
@@ -54,7 +58,8 @@ int Node_compare(Node node1, Node node2);
 const char* Node_getPath(Node n);
 
 /*
-  Returns the number of child directories n has.
+  Returns the number of child directories n has. n must be a directory,
+  not a file.
 */
 size_t Node_getNumChildren(Node n);
 
@@ -66,12 +71,14 @@ size_t Node_getNumChildren(Node n);
    If n does have such a child, and childID is not NULL, store the
    child's identifier in *childID. If n does not have such a child,
    store the identifier that such a child would have in *childID.
+
+   n must be a directory, not a file.
 */
 int Node_hasChild(Node n, const char* path, nodeType type);
 
 /*
    Returns the child Node of n with identifier childID, if one exists,
-   otherwise returns NULL.
+   otherwise returns NULL. n must be a directory, not a file.
 */
 Node Node_getChild(Node n, size_t childID);
 
@@ -89,6 +96,7 @@ Node Node_getParent(Node n);
     in which case returns ALREADY_IN_TREE
   * parent is unable to allocate memory to store new child link,
     in which case returns MEMORY_ERROR
+  * parents is a file, not a directory
  */
 int Node_linkChild(Node parent, Node child);
 
@@ -98,6 +106,8 @@ int Node_linkChild(Node parent, Node child);
 
   Returns PARENT_CHILD_ERROR if child is not a child of parent,
   and SUCCESS otherwise.
+
+  parent must be a directory node.
  */
 int Node_unlinkChild(Node parent, Node child);
 
@@ -113,6 +123,8 @@ int Node_unlinkChild(Node parent, Node child);
   Returns SUCCESS upon completion, or:
   MEMORY_ERROR if the new Node cannot be created,
   ALREADY_IN_TREE if parent already has a child with that path
+
+  parent must be a directory node, as specified by type.
 */
 int Node_addChild(Node parent, const char* dir, nodeType type);
 
